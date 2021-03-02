ce5e26c99a1538929340e105189acdbde1191417
Armin <becherarmin@gmail.com>
2021-03-02 19:56:55 +0100

    Fix tests

--- internal/client/client_test.go
+++ internal/client/client_test.go
@@ -3,21 +3,32 @@ package client_test
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"testing"
 
 	"github.com/becheran/roumon/internal/client"
 	"github.com/becheran/roumon/internal/model"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestPrint(t *testing.T) {
-	c := client.NewClient("localhost", 6060)
+func TestEmptyResponse(t *testing.T) {
+	const testport = 6062
+
+	// test server
+	go func() {
+		err := http.ListenAndServe(fmt.Sprintf("localhost:%d", testport), nil)
+		assert.Nil(t, err)
+	}()
+
+	testClient := client.NewClient("localhost", testport)
+
 	done := make(chan error)
 	routines := make(chan []model.Goroutine)
 
-	go c.Run(done, routines)
+	go testClient.Run(done, routines)
 	select {
 	case r := <-routines:
-		fmt.Println(r)
+		assert.Empty(t, r)
 	case <-done:
 		log.Fatal("Failed")
 	}
